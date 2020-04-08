4bee93c708501d0a0f3376e75253006e1ff5cced
EgonStep <041957Ab@>
2020-04-07 21:07:54 -0300

    Return response as JSON

--- consoles_app/my_app/console/models.py
+++ consoles_app/my_app/console/models.py
@@ -7,11 +7,15 @@ class Console(db.Model):
     name = db.Column(db.String(100))
     year = db.Column(db.Integer)
     price = db.Column(db.Float(asdecimal=True))
+    total_games = db.Column(db.Integer)
+    is_active = db.Column(db.Boolean)
 
-    def __init__(self, name, year, price):
+    def __init__(self, name, year, price, total_games, is_active):
         self.name = name
         self.year = year
         self.price = price
+        self.total_games = total_games
+        self.is_active = is_active
 
     # Return the console ID
     def __repr__(self):
--- consoles_app/my_app/console/views.py
+++ consoles_app/my_app/console/views.py
@@ -1,5 +1,4 @@
-import json
-from flask import Blueprint, abort
+from flask import Blueprint, abort, jsonify
 from flask_restful import Resource, reqparse
 from my_app.console.models import Console
 from my_app import api, db
@@ -10,6 +9,8 @@ parser = reqparse.RequestParser()
 parser.add_argument('name', type=str)
 parser.add_argument('year', type=int)
 parser.add_argument('price', type=float)
+parser.add_argument('total_games', type=int)
+parser.add_argument('is_active', type=bool)
 
 
 # Endpoint creation
@@ -23,37 +24,88 @@ class ConsoleAPI(Resource):
     def get(self, id=None, page=1):
         if not id:
             consoles = Console.query.paginate(page, 10).items
+            res = []
+            for con in consoles:
+                res.append({
+                    'id': con.id,
+                    'name': con.name,
+                    'year': con.year,
+                    'price': con.price,
+                    'total_games': con.total_games,
+                    'is_active': con.is_active
+                })
+            return jsonify(res)
         else:
             consoles = [Console.query.get(id)]
+            res = {}
+            for con in consoles:
+                res = {
+                    'id': con.id,
+                    'name': con.name,
+                    'year': con.year,
+                    'price': con.price,
+                    'total_games': con.total_games,
+                    'is_active': con.is_active
+                }
+            return jsonify(res)
         if not consoles:
             abort(404)  # HttpStatus
-
-        res = {}
-        for con in consoles:
-            res[con.id] = {
-                'name': con.name,
-                'year': con.year,
-                'price': str(con.price)
-            }
-
-        return json.dumps(res)
 
     def post(self):
         args = parser.parse_args()
         name = args['name']
         year = args['year']
         price = args['price']
+        total_games = args['total_games']
+        is_active = args['is_active']
 
-        con = Console(name, year, price)
+        con = Console(name, year, price, total_games, is_active)
         db.session.add(con)
         db.session.commit()
 
-        res = {con.id: {
+        res = {
+            'id': con.id,
             'name': con.name,
             'year': con.year,
-            'price': str(con.price)
-        }}
-        return json.dumps(res)
+            'price': con.price,
+            'total_games': con.total_games,
+            'is_active': con.is_active
+        }
+        return jsonify(res)
+
+    def delete(self, id):
+        con = Console.query.get(id)
+        db.session.delete(con)
+        db.session.commit()
+
+        res = {'id': id}
+        return jsonify(res)
+
+    def put(self, id):
+        con = Console.query.get(id)
+        args = parser.parse_args()
+        name = args['name']
+        year = args['year']
+        price = args['price']
+        total_games = args['total_games']
+        is_active = args['is_active']
+
+        con.name = name
+        con.year = year
+        con.price = price
+        con.total_games = total_games
+        con.is_active = is_active
+        db.session.commit()
+
+        res = {
+            'id': con.id,
+            'name': con.name,
+            'year': con.year,
+            'price': con.price,
+            'total_games': con.total_games,
+            'is_active': con.is_active
+        }
+        return jsonify(res)
 
 
 api.add_resource(
